27a7dc7bbf478f9775887d77ec5bdd01a4f721d5
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-09-17 14:50:11 +0700

    add isRegisterUsernameAvailable method

--- src/Domains/users/UserRepository.ts
+++ src/Domains/users/UserRepository.ts
@@ -3,6 +3,7 @@ import RegisterUser from './entities/RegisterUser';
 
 interface UserRepository {
   persist(registerUser: RegisterUser): Promise<string>
+  isRegisterUsernameAvailable(username: string) : Promise<boolean>
 }
 
 export default UserRepository;
