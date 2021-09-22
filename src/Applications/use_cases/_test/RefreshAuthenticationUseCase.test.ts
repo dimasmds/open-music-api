88a9dcf1976f759d6f4558ae4604d97a67e66361
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-09-22 17:48:44 +0700

    create RefreshAuthenticationUseCase

---
+++ src/Applications/use_cases/_test/RefreshAuthenticationUseCase.test.ts
@@ -0,0 +1,37 @@
+import RefreshAuthenticationUseCase from '../RefreshAuthenticationUseCase';
+import AuthTokenCreator from '../../../Domains/authentications/security/AuthTokenCreator';
+import AuthenticationRepository from '../../../Domains/authentications/repository/AuthenticationRepository';
+
+describe('RefreshAuthenticationUseCase', () => {
+  const mockAuthTokenCreator = <AuthTokenCreator>{};
+  const mockAuthenticationRepository = <AuthenticationRepository>{};
+
+  mockAuthTokenCreator.isSignatureValid = jest.fn(() => Promise.resolve(true));
+  mockAuthTokenCreator.getObjectPayload = jest.fn(() => Promise.resolve({ userId: 'user-123', iat: 123 }));
+  mockAuthTokenCreator.createAccessToken = jest.fn(() => Promise.resolve('new_access_token'));
+  mockAuthenticationRepository.isRefreshTokenRegistered = jest.fn(() => Promise.resolve(true));
+
+  const refreshAuthenticationUseCase = new RefreshAuthenticationUseCase({
+    authTokenCreator: mockAuthTokenCreator,
+    authenticationRepository: mockAuthenticationRepository,
+  });
+
+  describe('execute', () => {
+    it('should orchestrating refresh authentication flow correctly', async () => {
+      // Arrange
+      const payload = {
+        refreshToken: 'refresh_token',
+      };
+
+      // Action
+      const accessToken = await refreshAuthenticationUseCase.execute(payload);
+
+      // Assert
+      expect(accessToken).toEqual('new_access_token');
+      expect(mockAuthTokenCreator.isSignatureValid).toBeCalled();
+      expect(mockAuthTokenCreator.getObjectPayload).toBeCalledWith(payload.refreshToken);
+      expect(mockAuthenticationRepository.isRefreshTokenRegistered).toBeCalledWith('refresh_token');
+      expect(mockAuthTokenCreator.createAccessToken).toBeCalledWith({ userId: 'user-123' });
+    });
+  });
+});
