24206c56681a4ee4286b01045b5306b290a8cd2e
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-10-04 12:04:37 +0700

    Add create table song migration

---
+++ migrations/1633322208929_create-table-songs.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+    },
+  });
+
+  pgm.createConstraint('songs', 'fk.songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
