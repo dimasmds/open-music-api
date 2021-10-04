98a2a66e939eb83b66f7e153013ec37bfbe6954a
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-10-04 10:50:27 +0700

    Implement AlbumRepositoryPostgres

---
+++ src/Infrastructures/repository/AlbumRepositoryPostgres.ts
@@ -0,0 +1,77 @@
+import { Pool } from 'pg';
+import AlbumRepository from '../../Domains/albums/repository/AlbumRepository';
+import RepositoryDependencies from './definitions/RepositoryDependencies';
+import AlbumDetail from '../../Domains/albums/entities/AlbumDetail';
+import AlbumCreation from '../../Domains/albums/entities/AlbumCreation';
+import AlbumUpdate from '../../Domains/albums/entities/AlbumUpdate';
+
+class AlbumRepositoryPostgres implements AlbumRepository {
+  private pool: Pool;
+
+  private readonly idGenerator: Function;
+
+  constructor({ pool, idGenerator }: RepositoryDependencies) {
+    this.pool = pool;
+    this.idGenerator = idGenerator;
+  }
+
+  async deleteAlbum(albumId: string): Promise<void> {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1',
+      values: [albumId],
+    };
+
+    await this.pool.query(query);
+  }
+
+  async getAlbumById(albumId: string): Promise<AlbumDetail> {
+    const query = {
+      text: 'SELECT id, name, year FROM albums WHERE id = $1',
+      values: [albumId],
+    };
+
+    const result = await this.pool.query(query);
+
+    const rawAlbum = result.rows[0];
+
+    return new AlbumDetail({ ...rawAlbum, songs: null });
+  }
+
+  async isAlbumValid(albumId: string): Promise<boolean> {
+    const query = {
+      text: 'SELECT id FROM albums WHERE id = $1',
+      values: [albumId],
+    };
+
+    const result = await this.pool.query(query);
+
+    return !!result.rowCount;
+  }
+
+  async persist(albumCreation: AlbumCreation): Promise<string> {
+    const { name, year } = albumCreation;
+    const id = `album-${this.idGenerator()}`;
+
+    const query = {
+      text: 'INSERT INTO albums VALUES ($1, $2, $3) RETURNING id',
+      values: [id, name, year],
+    };
+
+    const result = await this.pool.query(query);
+
+    return result.rows[0].id;
+  }
+
+  async updateAlbum(albumUpdate: AlbumUpdate): Promise<void> {
+    const { id, name, year } = albumUpdate;
+
+    const query = {
+      text: 'UPDATE albums SET name = $2, year = $3 WHERE id = $1',
+      values: [id, name, year],
+    };
+
+    await this.pool.query(query);
+  }
+}
+
+export default AlbumRepositoryPostgres;
---
+++ src/Infrastructures/repository/_test/AlbumRepositoryPostgres.test.ts
@@ -0,0 +1,122 @@
+import AlbumRepositoryPostgres from '../AlbumRepositoryPostgres';
+import pool from '../../database/postgres/pool';
+import AlbumCreation from '../../../Domains/albums/entities/AlbumCreation';
+import AlbumsTableTestHelper from './_helper/AlbumsTableTestHelper';
+import AlbumDetail from '../../../Domains/albums/entities/AlbumDetail';
+import AlbumUpdate from '../../../Domains/albums/entities/AlbumUpdate';
+import AlbumRepository from '../../../Domains/albums/repository/AlbumRepository';
+
+describe('AlbumRepositoryPostgres', () => {
+  const fakeIdGenerator = () => '123';
+  const albumRepositoryPostgres = new AlbumRepositoryPostgres({
+    pool,
+    idGenerator: fakeIdGenerator,
+  });
+
+  beforeEach(async () => {
+    await AlbumsTableTestHelper.clean();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('persist', () => {
+    const album = new AlbumCreation();
+    album.name = 'Viva la vida';
+    album.year = 2020;
+
+    it('should persist album correctly', async () => {
+      // Action
+      await albumRepositoryPostgres.persist(album);
+
+      // Assert
+      const albums = await AlbumsTableTestHelper.findAlbumsById('album-123');
+      expect(albums).toHaveLength(1);
+      expect(albums[0].name).toEqual(album.name);
+      expect(albums[0].year).toEqual(album.year);
+    });
+
+    it('should return album id', async () => {
+      // Action
+      const albumId = await albumRepositoryPostgres.persist(album);
+
+      // Assert
+      expect(albumId).toEqual('album-123');
+    });
+  });
+
+  describe('getAlbumById', () => {
+    it('should return DetailAlbum correctly', async () => {
+      // Arrange
+      await AlbumsTableTestHelper.addAlbum({ id: 'album-321', name: 'Fix you', year: 1995 });
+
+      // Action
+      const album = await albumRepositoryPostgres.getAlbumById('album-321');
+
+      // Assert
+      expect(album).toBeInstanceOf(AlbumDetail);
+      expect(album.id).toEqual('album-321');
+      expect(album.year).toEqual(1995);
+      expect(album.name).toEqual('Fix you');
+      expect(album.songs).toEqual(null);
+    });
+  });
+
+  describe('updateAlbum', () => {
+    it('should update the album correctly', async () => {
+      // Arrange
+      await AlbumsTableTestHelper.addAlbum({ id: 'album-321', name: 'Fix you', year: 1995 });
+      const albumUpdate = new AlbumUpdate(<AlbumRepository>{});
+      albumUpdate.id = 'album-321';
+      albumUpdate.name = 'Viva la vida';
+      albumUpdate.year = 2000;
+
+      // Action
+      await albumRepositoryPostgres.updateAlbum(albumUpdate);
+
+      // Assert
+      const albums = await AlbumsTableTestHelper.findAlbumsById('album-321');
+      expect(albums[0].id).toEqual('album-321');
+      expect(albums[0].name).toEqual('Viva la vida');
+      expect(albums[0].year).toEqual(2000);
+    });
+  });
+
+  describe('isAlbumValid', () => {
+    it('should return false when album id is not valid', async () => {
+      const albumId = 'album-123';
+
+      // Action
+      const isValid = await albumRepositoryPostgres.isAlbumValid(albumId);
+
+      // Arrange
+      expect(isValid).toEqual(false);
+    });
+
+    it('should return true when album id is valid', async () => {
+      const albumId = 'album-123';
+      await AlbumsTableTestHelper.addAlbum({ id: albumId });
+
+      // Action
+      const isValid = await albumRepositoryPostgres.isAlbumValid(albumId);
+
+      // Arrange
+      expect(isValid).toEqual(true);
+    });
+  });
+
+  describe('deleteAlbum', () => {
+    it('should delete album correctly', async () => {
+      // Arrange
+      await AlbumsTableTestHelper.addAlbum({ id: 'album-123' });
+
+      // Action
+      await albumRepositoryPostgres.deleteAlbum('album-123');
+
+      // Assert
+      const albums = await AlbumsTableTestHelper.findAlbumsById('album-123');
+      expect(albums).toHaveLength(0);
+    });
+  });
+});
