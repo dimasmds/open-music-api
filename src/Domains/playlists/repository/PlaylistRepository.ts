64b6722a6715aab5a58d383b799de3c1de4f891d
Dimas Saputra <dimas.maulanads@gmail.com>
2021-10-10 18:37:48 +0700

    create GetDetailPlaylistUseCase

--- src/Domains/playlists/repository/PlaylistRepository.ts
+++ src/Domains/playlists/repository/PlaylistRepository.ts
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
 import PlaylistCreation from '../entities/PlaylistCreation';
 import Playlist from '../entities/Playlist';
+import PlaylistDetail from '../entities/PlaylistDetail';
 
 interface PlaylistRepository {
   persist(playlistCreation: PlaylistCreation) : Promise<string>
   getPlaylists(userId: string): Promise<Playlist[]>
+  getPlaylist(playlistId: string): Promise<PlaylistDetail>
+  isPlaylistIdValid(playlistId: string): Promise<boolean>
+  isAnOwnerPlaylist(playlistId: string, userId: string): Promise<boolean>
 }
 
 export default PlaylistRepository;
