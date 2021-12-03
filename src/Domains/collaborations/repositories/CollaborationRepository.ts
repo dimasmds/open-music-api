fa1648071fda2cf0ce798f7d7f7e1fe8a5141ee3
Dimas Saputra <dimas.maulanads@gmail.com>
2021-12-03 10:35:21 +0700

    change authorization access for put song in playlist

--- src/Domains/collaborations/repositories/CollaborationRepository.ts
+++ src/Domains/collaborations/repositories/CollaborationRepository.ts
@@ -5,6 +5,7 @@ import CollaborationDeletion from '../entities/CollaborationDeletion';
 interface CollaborationRepository {
   persist(collaborationCreation: CollaborationCreation) : Promise<string>
   delete(collaborationDeletion: CollaborationDeletion) : Promise<void>
+  isCollaboratorPlaylist(playlistId: string, userId: string) : Promise<boolean>
 }
 
 export default CollaborationRepository;
